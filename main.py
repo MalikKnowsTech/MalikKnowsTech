263ec7dafbb82fa805b184ea57bff2ee5c2b4b86
MalikKnowsTech <99135604+MalikKnowsTech@users.noreply.github.com>
2022-02-06 11:38:46 -0500

    Add files via upload
    Data Types Project

---
+++ main.py
@@ -0,0 +1,8 @@
+two_digit_number = input("Type a two digit number: ")
+
+first_character = two_digit_number[0]
+second_character = two_digit_number[1]
+a = int(first_character)
+b = int(second_character)
+
+print(a + b)
